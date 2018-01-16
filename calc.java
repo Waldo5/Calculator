9feca271fa199b147f2147ad1a0b833d8158f9dc
Waldo5 <33956263+Waldo5@users.noreply.github.com>
2018-01-16 19:13:03 +0300

    Create calc.java
    This contains the main method

---
+++ calc.java
@@ -0,0 +1,12 @@
+package calculator.pkg2;
+import javax.swing.*;
+public class calc {
+    public static void main(String[]args){
+            Calculator2 g = new Calculator2();
+            g.setSize(250,400);
+            
+            g.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            g.setVisible(true);
+            g.setResizable(false);
+}
+}
